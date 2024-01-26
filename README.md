2c8cb4fb39273ddf8953fe3279b63438d491279d
Miezhiko <Miezhiko@gmail.com>
2024-01-26 11:51:14 +0400

    get and display candles for some tickers
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- README.md
+++ README.md
@@ -5,3 +5,38 @@ for_ (pf ^. O.positions) $ \pos -> do
         ++ "\tQ: " ++ show ( pos ^. O.quantity ^. C.units )
         ++ "\tP: " ++ show ( pos ^. O.currentPrice ^. C.units )
 ```
+
+```haskell
+sinceEpoch ∷ UTCTime -> Int64
+sinceEpoch = floor ∘ nominalDiffTimeToSeconds
+                   ∘ utcTimeToPOSIXSeconds
+
+toTimestamp ∷ Int64 -> Timestamp
+toTimestamp s = build $ ( TS.seconds  .~ (s :: Int64) )
+                      ∘ ( TS.nanos    .~ (0 :: Int32) )
+
+runHistorical ∷ GrpcClient -> IO ()
+runHistorical g = do
+  now <- getCurrentTime
+  let Just daily  = maybeToEnum 5
+  let unixTime    = sinceEpoch now
+      weekSecs    = 60 * 60 * 24 * 7
+      tfrom       = toTimestamp $ unixTime - weekSecs
+      tto         = toTimestamp unixTime
+  let gcr = build $ ( MD.figi     .~ (T.pack "TCS00A107563") )
+                  ∘ ( MD.from     .~ tfrom )
+                  ∘ ( MD.to       .~ tto )
+                  ∘ ( MD.interval .~ daily )
+  cndls <- runGetCandles g gcr
+  for_ cndls $ \pos -> do
+    putStrLn $ show ( pos ^. MD.close )
+```
+
+```bash
+{units: 10 nano: 520000000}
+{units: 10 nano: 590000000}
+{units: 10 nano: 590000000}
+{units: 10 nano: 580000000}
+{units: 10 nano: 550000000}
+{units: 10 nano: 560000000}
+```
--- src/Historical.hs
+++ src/Historical.hs
@@ -4,21 +4,51 @@ module Historical
 
 import           Base
 
-import           Data.Foldable                  (for_)
+import           Data.Foldable                          (for_)
+import           Data.Int
 import           Data.ProtoLens.Message
-import qualified Data.Text                      as T
+import qualified Data.Text                              as T
+import           Data.Time
+import           Data.Time.Clock.POSIX
 
 import           Invest.Client
 import           Invest.Service.MarketData
 
 import           Proto.Invest.Marketdata
-import qualified Proto.Invest.Marketdata_Fields  as MD
+import qualified Proto.Invest.Marketdata_Fields         as MD
+
+import           Proto.Google.Protobuf.Timestamp
+import qualified Proto.Google.Protobuf.Timestamp_Fields as TS
 
 runGetCandles ∷ GrpcClient -> GetCandlesRequest -> IO [HistoricCandle]
 runGetCandles client gcr =
   runExceptT (getCandles client gcr) >>= \case
     Left err -> error ∘ show $ err
     Right cn -> pure cn
+
+sinceEpoch ∷ UTCTime -> Int64
+sinceEpoch = floor ∘ nominalDiffTimeToSeconds
+                   ∘ utcTimeToPOSIXSeconds
+
+toTimestamp ∷ Int64 -> Timestamp
+toTimestamp s = build $ ( TS.seconds  .~ (s :: Int64) )
+                      ∘ ( TS.nanos    .~ (0 :: Int32) )
+
+runHistorical ∷ GrpcClient -> IO ()
+runHistorical g = do
+  now <- getCurrentTime
+  let Just daily  = maybeToEnum 5
+  let unixTime    = sinceEpoch now
+      weekSecs    = 60 * 60 * 24 * 7
+      tfrom       = toTimestamp $ unixTime - weekSecs
+      tto         = toTimestamp unixTime
+  let gcr = build $ ( MD.figi     .~ (T.pack "TCS00A107563") )
+                  ∘ ( MD.from     .~ tfrom )
+                  ∘ ( MD.to       .~ tto )
+                  ∘ ( MD.interval .~ daily )
+  cndls <- runGetCandles g gcr
+  for_ cndls $ \pos -> do
+    putStrLn $ show ( pos ^. MD.close )
 
 --message GetCandlesRequest {
 --  string figi = 1;
@@ -48,12 +78,3 @@ message HistoricCandle {
   bool is_complete = 7; //Признак завершённости свечи. **false** значит, свеча за текущие интервал ещё сформирована не полностью.
 }
 -}
-
-runHistorical ∷ GrpcClient -> IO ()
-runHistorical g = do
-  let Just daily = maybeToEnum 5
-  let gcr = build $ ( MD.figi .~ (T.pack "XXX") )
-                  . ( MD.interval .~ daily )
-  cndls <- runGetCandles g gcr
-  for_ cndls $ \pos -> do
-    putStrLn $ show ( pos ^. MD.close )
